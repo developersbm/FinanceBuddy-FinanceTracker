a7007df35fc5a882009f8c625ecdbf961da2b71d
Sebastian Bastida Marin <122469079+developersbm@users.noreply.github.com>
2024-03-19 03:43:49 +0000

    FrontEnd Implementation Basic Features

--- frontend/src/Components/Chart/Chart.js
+++ frontend/src/Components/Chart/Chart.js
@@ -74,7 +74,7 @@ const ChartStyled = styled.div`
     box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
     padding: 1rem;
     border-radius: 20px;
-    height: 100%;
+    height: 90%;
 `;
 
 export default Chart
--- frontend/src/Components/Navigation/Navigation.js
+++ frontend/src/Components/Navigation/Navigation.js
@@ -11,7 +11,7 @@ function Navigation({active, setActive}) {
             <div className="user-con">
                 <img src={avatar} alt="" />
                 <div className="text">
-                    <h2>Mike</h2>
+                    <h2>Sebas</h2>
                     <p>Your Money</p>
                 </div>
             </div>
